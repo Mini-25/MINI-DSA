e73fc990397c252c9ee27ed117b652ce5a1ae1bf
Muhammad Moeez <157150149+Mini-25@users.noreply.github.com>
2024-10-17 13:54:09 +0500

    Update binarySearchTree.cpp

--- BINARY_SEARCH_TREE/binarySearchTree.cpp
+++ BINARY_SEARCH_TREE/binarySearchTree.cpp
@@ -1,9 +1,80 @@
-#include <iostream>
-#include "binarySearchTree.h"
+#include<iostream>
 using namespace std;
 
-int main() {
-    BinarySearchTree tree;
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value): data(value), left(nullptr), right(nullptr){}
+};
+class binarySearchTree{
+    private:
+        Node* root;
+        void inorder(Node* node){
+            if(node != nullptr){
+                inorder(node->left);
+                cout << node->data << " ";
+                inorder(node->right);
+            }
+        }
+        void preorder(Node* node){
+            if(node != nullptr){
+                cout << node->data << " ";
+                preorder(node->left);
+                preorder(node->right);
+            }
+        }
+        void postorder(Node* node){
+            if(node!= nullptr){
+                postorder(node->left);
+                postorder(node->right);
+                cout << node->data << " ";
+            }
+        }
+    public:
+    
+        binarySearchTree(): root(nullptr){}
+
+    void insert(int value){
+        Node* newNode = new Node(value);
+        if(root == nullptr)
+            root = newNode;
+        else {
+            Node* current = root;
+            Node* parent = nullptr;
+            while (current != nullptr)
+            {
+                parent = current;
+                if(value < current->data)
+                    current = current->left;
+                else
+                    current = current-> right;
+            }
+            if(value < parent->data)
+                parent->left = newNode;
+            else
+                parent->right = newNode;
+        }   
+    }
+    void inordertraversal() {
+            inorder(root);
+            cout<<endl;
+        }
+
+        void preordertraversal() {
+            preorder(root);
+            cout<<endl;
+        }
+
+        void postordertraversal() {
+            postorder(root);
+            cout<<endl;
+        }
+};
+
+int main(){
+    binarySearchTree tree;
 
     // Inserting the nodes into the tree
     tree.insert(50);
